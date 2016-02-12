a3cb1d97592a489ee665bd93f30785521a9282c3
Paulo Motta <pauloricardomg@gmail.com>
2016-02-12 12:26:28 -0300

    Test to reproduce CASSANDRA-8343

--- bootstrap_test.py
+++ bootstrap_test.py
@@ -88,6 +88,57 @@ class TestBootstrap(Tester):
         assert_almost_equal(size1, size2, error=0.3)
         assert_almost_equal(float(initial_size - empty_size), 2 * (size1 - float(empty_size)))
 
+    def keepalive_test(self):
+        cluster = self.cluster
+        keys = 10000
+
+        # Create a single node cluster
+        cluster.populate(1)
+        node1 = cluster.nodelist()[0]
+        node1.set_configuration_options(values={'streaming_socket_timeout_in_ms': 1000})
+        cluster.start(wait_other_notice=True, jvm_args=['-Dcassandra.dtest.sleep_during_stream_write=9999999999'])
+
+        session = self.patient_cql_connection(node1)
+        self.create_ks(session, 'ks', 1)
+        self.create_cf(session, 'cf', columns={'c1': 'text', 'c2': 'text'})
+
+        # record the size before inserting any of our own data
+        empty_size = node1.data_size()
+        debug("node1 empty size : %s" % float(empty_size))
+
+        insert_statement = session.prepare("INSERT INTO ks.cf (key, c1, c2) VALUES (?, 'value1', 'value2')")
+        execute_concurrent_with_args(session, insert_statement, [['k%d' % k] for k in range(keys)])
+
+        node1.flush()
+        node1.compact()
+        initial_size = node1.data_size()
+        debug("node1 size before bootstrapping node2: %s" % float(initial_size))
+
+        # Reads inserted data all during the bootstrap process. We shouldn't
+        # get any error
+        reader = self.go(lambda _: query_c1c2(session, random.randint(0, keys - 1), ConsistencyLevel.ONE))
+
+        # Bootstraping a new node
+        node2 = new_node(cluster)
+        node2.set_configuration_options(values={'streaming_socket_timeout_in_ms': 1000})
+        node2.start(wait_for_binary_proto=True)
+        node2.compact()
+
+        reader.check()
+        node1.cleanup()
+        debug("node1 size after cleanup: %s" % float(node1.data_size()))
+        node1.compact()
+        debug("node1 size after compacting: %s" % float(node1.data_size()))
+        time.sleep(.5)
+        reader.check()
+
+        debug("node2 size after compacting: %s" % float(node2.data_size()))
+
+        size1 = float(node1.data_size())
+        size2 = float(node2.data_size())
+        assert_almost_equal(size1, size2, error=0.3)
+        assert_almost_equal(float(initial_size - empty_size), 2 * (size1 - float(empty_size)))
+
     def simple_bootstrap_test_nodata(self):
         """
         @jira_ticket CASSANDRA-11010
